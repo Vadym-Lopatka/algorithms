5c161549d9d1af4a3f7b37d0f167aa2fe78e7064
Vadym-Lopatka <mirve09021990lik>
2019-03-22 11:25:30 +0200

    obvious fixes to MyStack implementation

--- src/main/java/com/vadym/lopatka/dataStructures/LiFoLinkedListImpl.java
+++ src/main/java/com/vadym/lopatka/dataStructures/LiFoLinkedListImpl.java
@@ -1,9 +1,12 @@
 package com.vadym.lopatka.dataStructures;
 
+/**
+ * Stack
+ */
 public class LiFoLinkedListImpl implements MyLinkedList {
     private int[] store;
     private int size;
-    private static final int defaultStoreSize = 10;
+    private static final int defaultStoreSize = 2;
 
     public LiFoLinkedListImpl() {
         this(defaultStoreSize);
@@ -16,14 +19,24 @@ public class LiFoLinkedListImpl implements MyLinkedList {
 
     @Override
     public void add(int element) {
-        int position = store.length - size - 1;
-        store[position] = element;
+        if (size >= store.length)
+            increaseStoreCapacity();
+
+        store[size] = element;
         size++;
+    }
+
+    private void increaseStoreCapacity() {
+        int newCapacity = store.length * 2;
+        int[] ints = new int[newCapacity];
+        System.arraycopy(this.store, 0, ints, 0, store.length);
+
+        this.store = ints;
     }
 
     @Override
     public int get() {
-        int index = store.length - size;
+        int index = size - 1;
         int theElement = store[index];
 
         removeElement(index);
@@ -36,10 +49,9 @@ public class LiFoLinkedListImpl implements MyLinkedList {
     }
 
     private void removeElement(int index) {
-        store[index]=0;
+        store[index] = 0;
         size--;
     }
-
 
 
 }
--- src/test/java/com/vadym/lopatka/dataStructures/LiFoLinkedListImplTest.java
+++ src/test/java/com/vadym/lopatka/dataStructures/LiFoLinkedListImplTest.java
@@ -26,7 +26,13 @@ public class LiFoLinkedListImplTest {
         linkedList.add(1);
         linkedList.add(11);
         linkedList.add(111);
+        linkedList.add(1111);
+        linkedList.add(11111);
+        linkedList.add(111111);
 
+        Assert.assertEquals(111111, linkedList.get());
+        Assert.assertEquals(11111, linkedList.get());
+        Assert.assertEquals(1111, linkedList.get());
         Assert.assertEquals(111, linkedList.get());
         Assert.assertEquals(11, linkedList.get());
         Assert.assertEquals(1, linkedList.get());
