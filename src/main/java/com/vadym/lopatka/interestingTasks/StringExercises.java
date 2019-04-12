a74e2b3d49aecf5c17593221ec7aff39fcbeb0b8
Vadym-Lopatka <mirve09021990lik>
2019-04-12 18:00:12 +0300

    change style

--- src/main/java/com/vadym/lopatka/interestingTasks/StringExercises.java
+++ src/main/java/com/vadym/lopatka/interestingTasks/StringExercises.java
@@ -1,21 +1,18 @@
 package com.vadym.lopatka.interestingTasks;
 
-import java.util.Optional;
-
 public class StringExercises {
 
     public static String reverseString(String strToReverse) {
-        return Optional.ofNullable(strToReverse)
-                .map(s -> {
+        if (strToReverse != null) {
+            char[] chars = strToReverse.toCharArray();
+            StringBuilder reversed = new StringBuilder(chars.length);
 
-                    char[] chars = s.toCharArray();
-                    StringBuilder reversedStr = new StringBuilder(chars.length);
+            for (int i = strToReverse.length() - 1; i >= 0; i--)
+                reversed.append(chars[i]);
 
-                    for (int i = s.length() - 1; i >= 0; i--) {
-                        reversedStr.append(chars[i]);
-                    }
-                    return reversedStr.toString();
-                })
-                .orElse("");
+            return reversed.toString();
+        }
+
+        return "";
     }
 }
