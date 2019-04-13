278e1d50dc638f1f6f8ba58310babdeea9d6ded4
Vadym-Lopatka <mirve09021990lik>
2019-04-13 21:16:52 +0300

    added base methods to my tree by TDD

--- src/main/java/com/vadym/lopatka/dataStructures/BinaryTree.java
+++ src/main/java/com/vadym/lopatka/dataStructures/BinaryTree.java
@@ -1,13 +1,15 @@
 package com.vadym.lopatka.dataStructures;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
 public class BinaryTree {
     private Node root;
 
+    /**
+     * @param value
+     * @return
+     */
     public boolean add(Integer value) {
         if (root == null)
             root = new Node(value);
@@ -18,26 +20,48 @@ public class BinaryTree {
         return true;
     }
 
-    private void addValueToNode(Node node, Integer value) {
-        if (value >= node.value) {
-            if (node.right != null)
-                addValueToNode(node.right, value);
-            else {
-                node.right = new Node(value, node);
-            }
-        } else {
-            if (node.left != null)
-                addValueToNode(node.left, value);
-            else {
-                node.left = new Node(value, node);
-            }
-        }
+    /**
+     * @param stream
+     * @return
+     */
+    public boolean addAll(IntStream stream) {
+        stream.forEach(this::add);
+        return true;
     }
 
+    /**
+     * @return the leftmost element
+     */
     public int getLowest() {
         return Optional.ofNullable(root)
                 .map(this::findLowest)
                 .orElseThrow(() -> new IllegalStateException("tree is empty"));
+    }
+
+    /**
+     * @return the rightmost element
+     */
+    public int getHighest() {
+        return Optional.ofNullable(root)
+                .map(this::findHighest)
+                .orElseThrow(() -> new IllegalStateException("tree is empty"));
+    }
+
+    private void addValueToNode(Node node, Integer value) {
+        if (value >= node.value) {
+
+            if (node.right != null)
+                addValueToNode(node.right, value);
+            else
+                node.right = new Node(value, node);
+
+        } else {
+
+            if (node.left != null)
+                addValueToNode(node.left, value);
+            else
+                node.left = new Node(value, node);
+        }
     }
 
     private int findLowest(Node node) {
@@ -47,28 +71,11 @@ public class BinaryTree {
         return node.value;
     }
 
-    public int getHighest() {
-        return Optional.ofNullable(root)
-                .map(this::findHighest)
-                .orElseThrow(() -> new IllegalStateException("tree is empty"));
-    }
-
     private int findHighest(Node node) {
         if (node.right != null)
             return findHighest(node.right);
 
         return node.value;
-    }
-
-    public boolean addAll(List<Integer> integers) {
-        integers = Optional.ofNullable(integers).orElse(new ArrayList<>());
-        integers.forEach(this::add);
-        return true;
-    }
-
-    public boolean addAll(IntStream stream) {
-        stream.forEach(this::add);
-        return true;
     }
 
     private class Node {
